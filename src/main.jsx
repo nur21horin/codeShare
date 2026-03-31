f6654cc0ee3b45c62c9cfcdcb4a138815263f540
Nur Mohammod <nurm98371@gmail.com>
2026-03-31 23:46:27 +0600

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Home from "./pages/Home/home.jsx";
+import Profile from "./pages/Profile/Profile.jsx";
+import Login from "./pages/Login/Login.jsx";
+import CreatePost from "./pages/CreatePost/CreatePost.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element:<App></App>,
+    element: <App></App>,
+  },
+  {
+    path: "/home",
+    element: <Home></Home>,
+  },
+  {
+    path: "/profile",
+    element: <Profile></Profile>,
+  },
+  {
+    path: "/login",
+    element: <Login></Login>,
+  },
+  {
+    path: "/createpost",
+    element: <CreatePost></CreatePost>,
   },
 ]);
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-   
-   <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
