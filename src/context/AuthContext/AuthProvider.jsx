1448e3de24bf04a78fbd36aab0f60f676794ef64
Nur Mohammod <nurm98371@gmail.com>
2026-04-03 01:23:45 +0600

    Add

--- src/context/AuthContext/AuthProvider.jsx
+++ src/context/AuthContext/AuthProvider.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { createUserWithEmailAndPassword, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { auth } from "../../Firebase/Firebase.init";
 
 const AuthProvider = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [loading, setLoading] = useState(true);
+  const registerUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-export default AuthProvider
+  const signInuser=(email,password)=>{
+    setLoading(true);
+    return signInWithEmailAndPassword(auth,email,password);
+  }
+  const resetPassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
+
+  return <div></div>;
+};
+
+export default AuthProvider;
