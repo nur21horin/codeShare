d34fa3b31aef145cb2052280e25a89c9c515eaff
Nur Mohammod <nurm98371@gmail.com>
2026-04-03 01:24:37 +0600

    Add

--- src/context/AuthContext/AuthProvider.jsx
+++ src/context/AuthContext/AuthProvider.jsx
@@ -1,4 +1,11 @@
-import { createUserWithEmailAndPassword, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../../Firebase/Firebase.init";
 
@@ -9,12 +16,25 @@ const AuthProvider = () => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
-  const signInuser=(email,password)=>{
+  const signInuser = (email, password) => {
     setLoading(true);
-    return signInWithEmailAndPassword(auth,email,password);
-  }
+    return signInWithEmailAndPassword(auth, email, password);
+  };
   const resetPassword = (email) => {
     return sendPasswordResetEmail(auth, email);
+  };
+  const signInGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  const updateUserProfile = (profile) => {
+    return updateProfile(auth.currentUser, profile);
   };
 
   return <div></div>;
