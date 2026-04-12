f997c4545f09fdc72fcbd0dd1766a1e47639596e
Nur Mohammod <nurm98371@gmail.com>
2026-04-12 16:52:48 +0600

    DD

---
+++ src/components/Help/Help.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Help = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Help</h1>
+      <p className="mt-2 text-gray-600">
+        Need assistance? Check out our frequently asked questions or contact our support team.
+      </p>
+    </div>
+  )
+}
+
+export default Help
