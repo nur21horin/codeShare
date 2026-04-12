e881e8a06dc13e85ece68639504546b3561c4876
Nur Mohammod <nurm98371@gmail.com>
2026-04-12 15:26:31 +0600

    AD

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -10,22 +10,37 @@ const Home = () => {
   const axiosSecure = useAxiosSecure();
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  // useEffect(() => {
+  //   axiosSecure.get("/posts")
+  //     .then(res => {
+  //       setPosts(res.data);
+  //     })
+  //     .catch(err => console.log(err))
+  //     .finally(() => setLoading(false));
+  // }, []);
 
   useEffect(() => {
-    axiosSecure.get("/posts")
-      .then(res => {
-        setPosts(res.data);
-      })
-      .catch(err => console.log(err))
-      .finally(() => setLoading(false));
-  }, []);
+  setLoading(true);
+
+  fetch(`http://localhost:5000/posts?page=${page}&limit=8`)
+    .then(res => res.json())
+    .then(data => {
+      setPosts(data.posts);
+      setTotalPages(data.totalPages);
+      setLoading(false);
+    });
+}, [page]);
 
   if (loading) {
     return <CardSkeleton></CardSkeleton>
   }
 
   return (
-    <div className=" mx-auto py-6 px-6 space-y-6 grid gap-6 
+    <div>
+      <div className=" mx-auto py-6 px-6 space-y-6 grid gap-6 
   grid-cols-1 
   sm:grid-cols-2 
   md:grid-cols-3 
@@ -34,6 +49,41 @@ const Home = () => {
         <PostCard key={post._id} post={post} />
       ))}
       <CreatePostBox></CreatePostBox>
+      
+    </div>
+    <div className="flex justify-center mt-6 gap-2">
+
+  {/* PREV */}
+  <button
+    disabled={page === 1}
+    onClick={() => setPage(prev => prev - 1)}
+    className="btn btn-sm"
+  >
+    Prev
+  </button>
+
+  {/* PAGE NUMBERS */}
+  {[...Array(totalPages).keys()].map(num => (
+    <button
+      key={num}
+      onClick={() => setPage(num + 1)}
+      className={`btn btn-sm ${
+        page === num + 1 ? "btn-primary" : ""
+      }`}
+    >
+      {num + 1}
+    </button>
+  ))}
+
+  {/* NEXT */}
+  <button
+    disabled={page === totalPages}
+    onClick={() => setPage(prev => prev + 1)}
+    className="btn btn-sm"
+  >
+    Next
+  </button>
+</div>
     </div>
   );
 };
